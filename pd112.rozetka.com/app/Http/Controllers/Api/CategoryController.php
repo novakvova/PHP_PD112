32bd243edf2ffe2acfd7f88334f21b22cda90e05
novakvova <novakvova@gmail.com>
2023-12-13 17:43:58 +0200

    Add new table and migration

--- pd112.rozetka.com/app/Http/Controllers/Api/CategoryController.php
+++ pd112.rozetka.com/app/Http/Controllers/Api/CategoryController.php
@@ -3,16 +3,18 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\Categories;
 use Illuminate\Http\Request;
 
 class CategoryController extends Controller
 {
     public function getList() {
-        $data = [
-            ['id' => 1, 'name' => 'Коти'],
-            ['id' => 2, 'name' => 'Собачка'],
-            ['id' => 3, 'name' => 'Мишка'],
-        ];
+        $data = Categories::all();
+//        $data = [
+//            ['id' => 1, 'name' => 'Коти'],
+//            ['id' => 2, 'name' => 'Собачка'],
+//            ['id' => 3, 'name' => 'Мишка'],
+//        ];
         return response()->json($data)
             ->header('Content-Type', 'application/json; charset=utf-8');
     }
