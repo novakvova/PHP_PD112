8b1d3941edfd912a8d8c9baa6da95729ff301d39
novakvova <novakvova@gmail.com>
2023-12-15 16:09:50 +0200

    Begin working upload image

--- pd112.rozetka.com/app/Http/Controllers/Api/CategoryController.php
+++ pd112.rozetka.com/app/Http/Controllers/Api/CategoryController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use App\Models\Categories;
 use Illuminate\Http\Request;
+use Intervention\Image\ImageManager;
+use Intervention\Image\Drivers\Gd\Driver;
 
 class CategoryController extends Controller
 {
@@ -17,5 +19,25 @@ class CategoryController extends Controller
 //        ];
         return response()->json($data)
             ->header('Content-Type', 'application/json; charset=utf-8');
+    }
+
+    public function create(Request $request) {
+        $image = $request->file("image");
+        $imageName = uniqid().".webp";
+        $sizes = [50,150,300,600,1200];
+        // create image manager with desired driver
+        $manager = new ImageManager(new Driver());
+
+        foreach ($sizes as $size) {
+            $fileSave = $size."_".$imageName;
+            // read image from file system
+            $imageRead = $manager->read($image);
+            // resize image proportionally to 300px width
+            $imageRead->scale(width: $size);
+            // save modified image in new format
+            $path=public_path('upload/'.$fileSave);
+            $imageRead->toWebp()->save($path);
+        }
+        return response()->json();
     }
 }
--- pd112.rozetka.com/config/app.php
+++ pd112.rozetka.com/config/app.php
@@ -160,6 +160,7 @@ return [
          * Package Service Providers...
          */
 
+
         /*
          * Application Service Providers...
          */
@@ -183,6 +184,7 @@ return [
 
     'aliases' => Facade::defaultAliases()->merge([
         // 'Example' => App\Facades\Example::class,
+
     ])->toArray(),
 
 ];
